2b14f22c789d6663fab2b88b3378d0827b1548aa
sylvine <sylvinefau@gmail.com>
2018-03-30 15:57:41 +0200

    fonction avec alert

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 'use strict';
 var mail = document.getElementById('email');
 var nom = document.getElementById('lastname');
-var prenom = document.getElementById('firtname');
+var prenom = document.getElementById('firstname');
 var motDePasse = document.getElementById('password');
 var confirmMdP = document.getElementById('passwordconfirm');
 var button = document.getElementById('go');
 
-
-
-
-
 button.onclick = function onFormSubmit(event) {
     event.preventDefault(); // indispensable, sinon la page est rechargée
-    !!!
-    console.log(event.target + ' a recu l\'évènement submit');
-    // garder ça à la fin de la fonction indispensable, sinon la pas est
-    // rechargée !!!
-    return false;
+
+    if (typeof(mail.value) == "string" &&
+    typeof (nom.value) == "string" &&
+    typeof(prenom.value) == "string" &&
+    motDePasse.value === confirmMdP.value 
+    ) {
+    console.log('ok'); 
+    window.alert('bienvenue ' + nom);  
     }
+    else {
+    window.alert('tous les champs ne sont pas rempli correctement');
+    }
+    console.log(event.target + ' a recu l\'évènement submit');
+    // garder ça à la fin de la fonction indispensable, sinon la page est
+    // pas rechargée !!!
+    // return false;
+};
